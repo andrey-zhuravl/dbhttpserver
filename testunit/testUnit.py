b9eb77ecab2a2667d8a44da08bd8885b9198b8a3
voyagerdva <mariner-9@yandex.ru>
2019-10-05 14:23:28 +0300

    fixtures & tests

--- testunit/testUnit.py
+++ testunit/testUnit.py
@@ -1,27 +1,43 @@
 import unittest
-
+# from http.server import BaseHTTPRequestHandler, HTTPServer
 from fixtures import fixtures as fx
-from myQuickSort import myQuickSort as qs
+from dbhttpserver.dbhttpserver import *
 import requests
 
-class myTestQuickSort(unittest.TestCase):
+class myTestHTTPRequests(unittest.TestCase):
+    #
+    # def test_reuestsToHTTPServerURL(self):
+    #     (myURL, myData, myHeader, codeSuccessful) = fx.fixturePostUserID()
+    #     # my = MyServer()
+    #     req = requests.post(myURL, myData)
+    #     print(myURL)
+    #     self.assertEqual(req.url, myURL)
 
-    def test_reuestsToHTTPServer(self):
-        (httpURL, jsonData, base, sepEtalon, low, high) = fx.fixturePartShuffleReady()
-        (listResult, sepPart) = qs.partition(listOriginal, base, low, high)
-        self.assertEqual(listResult, listEtalon)
-        self.assertEqual(sepEtalon, sepPart)
+    def test_reuestsToHTTPServerCODE200(self):
+        (myURL, myData, myHeader, codeSuccessful) = fx.fixturePostUserID()
+        myServer = MyServer()
+        req = requests.post(myURL, myData)
 
+        print(req.status_code)
+        print(codeSuccessful)
+        print(myServer.do_POST())
 
-        r = requests.post('http://httpbin.org/post', json={"key": "value"})
-        r.status_code
-        r.json()
+        self.assertEqual(req.status_code, codeSuccessful)
+    #
+    # def test_reuestsToHTTPServerJSON(self):
+    #     (myURL, myData, myHeader, codeSuccessful) = fx.fixturePostUserID()
+    #     myServer = MyServer()
+    #     print(myServer.do_POST())
+    #     req = requests.post(myURL, myData)
+    #     self.assertEqual(req.json, myData)
 
-        url = "http://localhost:8080"
-        data = {'sender': 'Alice', 'receiver': 'Bob', 'message': 'We did it!'}
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        r = requests.post(url, data=json.dumps(data), headers=headers)
-        requests.post
+        # r.status_code
+        # r.json()
+
+        # url = "http://localhost:8080"
+        # data = {'sender': 'Alice', 'receiver': 'Bob', 'message': 'We did it!'}
+        # headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        # r = requests.post(url, data=json.dumps(data), headers=headers)
 
         # 1. путь
         # 2. тело в формате json
